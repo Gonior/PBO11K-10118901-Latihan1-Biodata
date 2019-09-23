3908d4312ce06efe6172448cf7127dde8a0e8abb
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-23 18:14:21 +0700

    proses push ke github

---
+++ src/pbo11k/pkg10118901/latihan1/biodata/PBO11K10118901Latihan1Biodata.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA         : Dedi Cahya
+ * KELAS        : IF-11-K
+ * NIM          : 10118901
+ * DESKRIPSI    : Program ini untuk menampilkan biodata
+ */
+public class PBO11K10118901Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments  
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR"+"\n"+"--------------------------");
+        System.out.println("NAMA\t\t: Dedi Cahya");
+        System.out.println("ALAMAT\t\t: Jl. Punclut atas no. 12A");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 14 Desember 1997");
+        System.out.println("-----------------------------");
+        System.out.println("HOBI\t\t: Tidur & Game");
+        System.out.println("MAKUS\t\t: Mie Instan");
+        System.out.println("MIKUS\t\t: Susu");
+    }
+    
+}
